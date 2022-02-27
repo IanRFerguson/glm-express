e71fe5717ddee32bc7fa9cb49eaf9c0177642a5a
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-02-26 22:59:36 -0800

    Create glm_express.py

---
+++ glm_express.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+"""
+About this Class
+"""
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+from subject.subject import Subject
