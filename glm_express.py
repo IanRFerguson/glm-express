8391b8d5ce7b2aa74147507ce3d0c7ab75af6bb0
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-03-14 16:05:14 -0700

    updated class wrapper

--- glm_express.py
+++ glm_express.py
@@ -9,3 +9,4 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
 
 from subject.subject import Subject
+from group_level.group import GroupLevel
