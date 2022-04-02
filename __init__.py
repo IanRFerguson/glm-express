c04da69fa5fd41d07784fd1fc01ff05fad8b3074
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-04-02 10:27:16 -0700

    reformatted for publication (!!)

---
+++ __init__.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+"""
+GLM Express is a lightweight, object-oriented approach
+to first- and second-level modeling of functional neuroimaging data.
+For a primer and demo, see github.com/IanRFerguson/glm-express
+"""
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+__version__ = "1.0.5"
+
+from glm_express.aggregator.aggregator import Aggregator
+from glm_express.group_level.group import GroupLevel
+from glm_express.subject.subject import Subject
