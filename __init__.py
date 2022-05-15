b3d37d072a5032d9786c370d13bcacfb82481582
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-05-15 09:48:21 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 
 from glm_express.aggregator.aggregator import Aggregator
 from glm_express.group_level.group import GroupLevel
